c9696611ed6ae253d7b66f34ad8fc7276b90b89a
MariusKlug <marius.s.klug@gmail.com>
2021-08-10 16:33:53 +0200

    Renamed the functions to include the "zapline-plus" name

--- clean_data_with_zapline_plus_eeglab_wrapper.m
+++ clean_data_with_zapline_plus_eeglab_wrapper.m
@@ -1,5 +1,5 @@
-function [EEG, plothandles] = clean_data_with_zapline_eeglab_wrapper(EEG, zaplineConfig)
-% super small wrapper for zapline that takes an EEGLAB EEG struct and a config as input and makes sure all info is
+function [EEG, plothandles] = clean_data_with_zapline_plus_eeglab_wrapper(EEG, zaplineConfig)
+% Super small wrapper for zapline-plus that takes an EEGLAB EEG struct and a config as input and makes sure all info is
 % stored, passes the plothandles out to be saved on disk. The config input is optional, otherwise default parameters are
 % used. The used parameters are stored in EEG.etc.zapline.config and can be re-entered into the function to reproduce
 % the cleaning. For detailed information about the parameters, see the clean_data_with_zapline function.
@@ -13,16 +13,16 @@ function [EEG, plothandles] = clean_data_with_zapline_eeglab_wrapper(EEG, zaplin
 %   [EEG, plothandles] = clean_data_with_zapline_eeglab_wrapper(EEG,zaplineConfig); EEG.etc.zapline
 % 
 % 
-% see also: clean_data_with_zapline
+% see also: clean_data_with_zapline_plus
 % 
 % Author: Marius Klug, 2021
 
 if ~exist('zaplineConfig','var')
     % run zapline subfunction with default parameters
-    [EEG.data, EEG.etc.zapline.config, EEG.etc.zapline.analyticsResults, plothandles] = clean_data_with_zapline(EEG.data, EEG.srate);
+    [EEG.data, EEG.etc.zapline.config, EEG.etc.zapline.analyticsResults, plothandles] = clean_data_with_zapline_plus(EEG.data, EEG.srate);
 else
     % run zapline subfunction with config parameters
-    [EEG.data, EEG.etc.zapline.config, EEG.etc.zapline.analyticsResults, plothandles] = clean_data_with_zapline(EEG.data, EEG.srate, zaplineConfig);
+    [EEG.data, EEG.etc.zapline.config, EEG.etc.zapline.analyticsResults, plothandles] = clean_data_with_zapline_plus(EEG.data, EEG.srate, zaplineConfig);
 end
 
 
